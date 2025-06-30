9104e66e762371c73d7b3e28bd6dba947d42f7ff
mulkraj <mulkraj360@hotmail.com>
2025-06-30 06:57:27 +0100

    Initial commit. ASP.NET Core 9.0 project with Entity Framework and Minimal APIs.

---
+++ Endpoints/ShoppingEndpoints.cs
@@ -0,0 +1,23 @@
+
+
+using mayden.shoppingapp.data;
+using mayden.shoppingapp.models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public static class ShoppingEndpoints
+{
+    public static void AddShoppingEndpoints(this WebApplication app)
+    {
+        app.MapGet("/products", async (AppDbContext db) =>
+         await db.Products.ToListAsync()).WithName("GetProducts").WithOpenApi();
+
+        app.MapPost("/products/add", async (Product product, AppDbContext db) =>
+        {
+            db.Products.Add(product);
+            await db.SaveChangesAsync();
+            return Results.Created($"/products/{product.Id}", product);
+        }).WithName("AddProduct").WithOpenApi();
+    }   
+}
